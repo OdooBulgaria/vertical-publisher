70521ffa051f1ea9d0cbd97e61c75694cb80384f
Valentin Thirion <vt@abakusitsolutions.eu>
2017-06-26 09:07:07 +0200

    [ADD] Added manifest

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Vertical Publisher",
+
+    'summary': """
+    """,
+
+    'description': """
+        Vertical Publisher Management
+        
+        
+        
+        This module has been developed by Valentin Thirion @ AbAKUS it-solutions.
+    """,
+
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+
+    'category': 'Sale',
+    'version': '10.0.1.0',
+
+    'depends': [
+        'base',
+        'sale'
+    ],
+
+    'data': [
+    ],
+}
