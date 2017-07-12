9a058a21c3c11c7d0c573df8b4f3cace0868cce5
jasonpindat <j.pindat@hotmail.fr>
2017-07-12 09:42:51 +0200

    [ADD] publisher
    Added boolean fields in partner

--- publisher/models/res_partner.py
+++ publisher/models/res_partner.py
@@ -9,6 +9,20 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     agency = fields.Boolean(string="Is an Agency")
+
+    toolbox = fields.Boolean(string="Toolbox / Guide")
+    fed = fields.Boolean(string="FED Invitation")
+    fleet_dating = fields.Boolean(string="Fleet Dating Invitation")
+    footy = fields.Boolean(string="Fleet Owner Of The Year Invit.")
+    fi_awards = fields.Boolean(string="Fleet Innovation Awards Invit.")
+    awards_be = fields.Boolean(string="Awards (BE) Invitation")
+    network = fields.Boolean(string="Network Invitation")
+    l2ffa = fields.Boolean(string="LFFA (LU) Invitation")
+    experts_table = fields.Boolean(string="Experts Table Invitation")
+    test_drive = fields.Boolean(string="Test Drive Invitation")
+    fleet_dealer = fields.Boolean(string="Fleet Dealer")
+    newsletter = fields.Boolean(string="Newsletter Marketing Subscrib.")
+
     production_line_count = fields.Integer(string="Production Line Count", compute='_compute_production_line_count')
     
     @api.one
