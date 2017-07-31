122a294c207f4fdef46958711f044d2dd01280c4
jasonpindat <j.pindat@hotmail.fr>
2017-07-31 10:20:54 +0200

    [CHG] publisher
    Changed subscription product / quantity management

--- publisher/models/sale_subscription_line.py
+++ publisher/models/sale_subscription_line.py
@@ -10,3 +10,25 @@ class SaleSubscriptionLine(models.Model):
 
     partner_shipping_id = fields.Many2one('res.partner', string="Shipping Address")
     language_id = fields.Many2one('publisher.language', string="Language")
+
+    @api.onchange('product_id', 'quantity')
+    def onchange_product_id(self):
+        domain = super(SaleSubscriptionLine, self).onchange_product_id()
+        contract = self.analytic_account_id
+        company_id = contract.company_id.id
+        pricelist_id = contract.pricelist_id.id
+        context = dict(self.env.context, company_id=company_id, force_company=company_id, pricelist=pricelist_id, quantity=self.quantity)
+
+        if self.product_id:
+            partner = contract.partner_id.with_context(context)
+            if partner.lang:
+                context.update({'lang': partner.lang})
+
+            product = self.product_id.with_context(context)
+
+            self.price_unit = product.lst_price
+
+            if self.uom_id.id != product.uom_id.id:
+                self.price_unit = product.uom_id._compute_price(self.price_unit, self.uom_id)
+
+        return domain
