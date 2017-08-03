1791f4de7cff8a0ceb5b17888cbd5755ca42da8b
jasonpindat <j.pindat@hotmail.fr>
2017-08-03 16:20:04 +0200

    [CHG] publisher
    Added customer, shipping address, subscription name & language to
    invoice from subscription

--- publisher/models/sale_subscription.py
+++ publisher/models/sale_subscription.py
@@ -17,9 +17,21 @@ class SaleSubscription(models.Model):
 
         vals['name'] = '\n'.join(filter(None, [
             line.name,
-            _('Unit Price : ')+str(line.price_unit)+line.analytic_account_id.currency_id.symbol if line.quantity != 1 else '',
+            _('Customer : ')+self.partner_customer_id.name if self.partner_customer_id else '',
+            _('Shipping Address : ')+', '.join(filter(None, [
+                line.partner_shipping_id.name or '',
+                line.partner_shipping_id.street or '',
+                line.partner_shipping_id.street2 or '',
+                line.partner_shipping_id.city or '',
+                line.partner_shipping_id.state_id.name or '',
+                line.partner_shipping_id.zip or '',
+                line.partner_shipping_id.country_id.name or '',
+            ])) if line.partner_shipping_id else '',
+            _('Subscription Reference : ')+self.name,
+            _('Language : ')+line.language_id.name if line.language_id else '',
+            _('Unit Price : ')+str(line.price_unit)+self.currency_id.symbol if line.quantity != 1 else '',
             _('Quantity : ')+str(line.quantity) if line.quantity != 1 else '',
-            _('Price : ')+str(line.quantity*line.price_unit)+line.analytic_account_id.currency_id.symbol+(' - '+str(line.discount)+_(' % customer discount') if line.discount>0 else ''),
+            _('Price : ')+str(line.quantity*line.price_unit)+self.currency_id.symbol+(' - '+str(line.discount)+_(' % customer discount') if line.discount>0 else ''),
         ]))
 
         return vals
