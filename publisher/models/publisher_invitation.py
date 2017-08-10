a36b8c15167bf0c285b56a01e1b2cb62db42838f
jasonpindat <j.pindat@hotmail.fr>
2017-08-10 17:17:46 +0200

    [ADD] publisher
    Added button to display invited contacts for a specific invitation

--- publisher/models/publisher_invitation.py
+++ publisher/models/publisher_invitation.py
@@ -12,3 +12,8 @@ class Invitation(models.Model):
     name = fields.Char(string='Name', copy=False, index=True, required=True)
     active = fields.Boolean(string='Is Active', copy=False, default=True)
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id, required=True)
+    partner_count = fields.Integer(string="Partner Count", compute='_compute_partner_count')
+
+    @api.one
+    def _compute_partner_count(self):
+        self.partner_count = len(self.env['res.partner'].search([('invitation_ids.id', '=', self.id)]))
